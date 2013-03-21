58656545826cb9739ea622bc52ed587697f4f017
Drew <drew@gradientx.com>
2013-03-21 12:41:12 -0700

    Updating to the new header file

--- library/VwJniBinding.cc
+++ library/VwJniBinding.cc
@@ -6,14 +6,14 @@
 #include "../vowpalwabbit/vw.h"
 #include "../vowpalwabbit/constant.h"
 
-#include "com_gradientx_vw_VwJniBinding.h"
+#include "com_gradientx_vw_JniVowpalWabbit.h"
 using namespace std;
  
 /* global VW instance */
-#define VW_NUM com_gradientx_vw_VwJniBinding_VW_NUM
+#define VW_NUM com_gradientx_vw_JniVowpalWabbit_VW_NUM
 vw vw_arr[VW_NUM];
 
-JNIEXPORT void JNICALL Java_com_gradientx_vw_VwJniBinding_initialize(JNIEnv *env, jobject, jint id, jstring initStr)
+JNIEXPORT void JNICALL Java_com_gradientx_vw_JniVowpalWabbit_initialize(JNIEnv *env, jobject, jint id, jstring initStr)
 {
   char *str;
     str = (char*) (env)->GetStringUTFChars(initStr, NULL);
@@ -24,7 +24,7 @@ JNIEXPORT void JNICALL Java_com_gradientx_vw_VwJniBinding_initialize(JNIEnv *env
   (env)->ReleaseStringUTFChars(initStr, str);
 }
 
-JNIEXPORT jfloat JNICALL Java_com_gradientx_vw_VwJniBinding_read_1example(JNIEnv *env, jobject, jint id, jstring exStr)
+JNIEXPORT jfloat JNICALL Java_com_gradientx_vw_JniVowpalWabbit_read_1example(JNIEnv *env, jobject, jint id, jstring exStr)
 {
   char *str;
   str = (char*) (env)->GetStringUTFChars(exStr, NULL);
@@ -43,12 +43,12 @@ JNIEXPORT jfloat JNICALL Java_com_gradientx_vw_VwJniBinding_read_1example(JNIEnv
   return ret;
 }
 
-JNIEXPORT void JNICALL Java_com_gradientx_vw_VwJniBinding_finish(JNIEnv *, jobject, jint id)
+JNIEXPORT void JNICALL Java_com_gradientx_vw_JniVowpalWabbit_finish(JNIEnv *, jobject, jint id)
 {
   VW::finish(vw_arr[id]);
 }
 
-JNIEXPORT jfloatArray JNICALL Java_com_gradientx_vw_VwJniBinding_get_1coef(JNIEnv *env, jobject, jint id, jint size, jint constIdx)
+JNIEXPORT jfloatArray JNICALL Java_com_gradientx_vw_JniVowpalWabbit_get_1coef(JNIEnv *env, jobject, jint id, jint size, jint constIdx)
 {
   jfloatArray result;
   result = (env)->NewFloatArray(size);
@@ -74,7 +74,7 @@ JNIEXPORT jfloatArray JNICALL Java_com_gradientx_vw_VwJniBinding_get_1coef(JNIEn
 void dump_regressor(vw& all, string reg_name, bool as_text, bool reg_vector);
 void read_vector(vw& all, const char* file, bool& initialized, bool reg_vector);
 
-JNIEXPORT void JNICALL Java_com_gradientx_vw_VwJniBinding_dump_1regressor
+JNIEXPORT void JNICALL Java_com_gradientx_vw_JniVowpalWabbit_dump_1regressor
 (JNIEnv *env, jobject, jint id, jstring fn)
 {
   char *str;
@@ -88,7 +88,7 @@ JNIEXPORT void JNICALL Java_com_gradientx_vw_VwJniBinding_dump_1regressor
   (env)->ReleaseStringUTFChars(fn, str);
 }
 
-JNIEXPORT void JNICALL Java_com_gradientx_vw_VwJniBinding_load_1regressor
+JNIEXPORT void JNICALL Java_com_gradientx_vw_JniVowpalWabbit_load_1regressor
 (JNIEnv *env, jobject, jint id, jstring fn)
 {
   char *str;
@@ -103,7 +103,7 @@ JNIEXPORT void JNICALL Java_com_gradientx_vw_VwJniBinding_load_1regressor
   (env)->ReleaseStringUTFChars(fn, str);
 }
 
-JNIEXPORT void JNICALL Java_com_gradientx_vw_VwJniBinding_set_1coef
+JNIEXPORT void JNICALL Java_com_gradientx_vw_JniVowpalWabbit_set_1coef
 (JNIEnv *env, jobject, jint id, jfloatArray coef, jint constIdx)
 {
   vw& vwx=vw_arr[id];
